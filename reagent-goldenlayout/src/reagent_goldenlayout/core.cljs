b3877a0331ecea3c75e234a8874b1f91d2f3f0c9
Mohan <Mohankumarcm@gmail.com>
2018-10-07 02:20:50 +0800

    intial commit

--- reagent-goldenlayout/src/reagent_goldenlayout/core.cljs
+++ reagent-goldenlayout/src/reagent_goldenlayout/core.cljs
@@ -10,14 +10,83 @@
 ;;:content [{ :type 'row', :content[{ :type'react-component', :component 'test-component', :props { :label 'A' } },{ :type 'column', :content[{ :type'react-component', :component 'test-component', :props { :label 'B' } },{ :type'react-component', :component 'test-component', :props { :label 'C' } }] }] }]
 (def js-object (clj->js {:content [{ :type "row", :content[{ :type"react-component", :component "test-component", :props { :label "A" } },{ :type "column", :content[{ :type"react-component", :component "test-component", :props { :label "B" } },{ :type"react-component", :component "test-component", :props { :label "C" } }] }] }] }))
 
+(def config  { :content [{ :type "row", :content [{ :title "Users", :type"react-component", :component "user-list" },{ :title "User Detail", :type"react-component", :component "user-detail" }] }] })
+(def config-js (clj->js config))
+
 (enable-console-print!)
 (println "mhan")
 
 (println js-object)
 
-(def t2 (new js/GoldenLayout js-object))
+(def t2 (new js/GoldenLayout config-js))
 
 (println t2)
+
+(def User (r/create-class
+                {:get-initial-state (fn [this]
+                                      (.. this -props -userData))
+
+                 :selectUser (fn [this]
+                               (let [gl-ev (.. this -props -glEventHub)
+                                     st (.-state this)]
+                                 (.emit gl-ev "user-select" st)
+                                 ))
+
+                 :render
+                                    (fn [this]
+                                      [:li {:onClick #(.selectUser this)} (.. this -state -name)])
+                 }))
+
+(def UserDetail (r/create-class
+                  {:component-will-mount (fn [this]
+                                           (let [gl-ev (.. this -props -glEventHub)]
+                                             (.on gl-ev "user-select" (.setUser this))))
+                   :component-will-unmount (fn [this]
+                                             (let [gl-ev (.. this -props -glEventHub)]
+                                               (.off gl-ev "user-select" (.setUser this))))
+                   :setUser (fn [ud]
+                              (this-as this (.setState this ud )))
+
+                   :render (fn [this]
+                             ((if (.state this)
+                               (let [imgUrl (str "https://s3-us-west-2.amazonaws.com/s.cdpn.io/152047/" (.. this -state -img)) ]
+                                 [:div.userdetails
+                                  [:img {:src imgUrl :width 100 :height 100}]
+                                  [:h2 (.. this -state -name)]
+                                  [:p (.. this -state -street)]
+                                  ]
+                                 )
+                               ([:div.userdetails "No User Selected"]))))
+
+                   }))
+
+
+(def UserList (r/create-class
+            {:get-initial-state (fn [this]
+                                  {:users  [
+                                            { :name "Jackson Turner", :street "217 Tawny End", :img "men_1.jpg" },
+                                            { :name "Megan Perry", :street "77 Burning Ramp", :img "women_1.jpg" },
+                                            { :name "Ryan Harris", :street "12 Hazy Apple Route", :img "men_2.jpg" },
+                                            { :name "Jennifer Edwards", :street "33 Maple Drive", :img "women_2.jpg" },
+                                            { :name "Noah Jenkins", :street "423 Indian Pond Cape", :img "men_3.jpg" }
+                                            ]})
+
+             :render
+             (fn [this]
+               [:ul.userlist (.map (.. this -state -users)
+                                   (fn [user]
+                                       [User {:key (.-name user)
+                                              :userData user
+                                              :glEventHub (.. this -props -glEventHub)}]
+                ))]
+
+              )
+             }))
+
+
+
+
+
 
 ;;(.. js/object -prop1 -prop2 -prop3) ;; JS output: object.prop1.prop2.prop3;
 ;;(aget js/object "prop1" "prop2" "prop3") ;; JS output: object["prop1"]["prop2"]["prop3"];
@@ -37,8 +106,13 @@
 
 
 
-
 (defn home-page []
+  (println "I am in home page")
+  (.registerComponent t2 "user-list" UserList)
+  (.registerComponent t2 "user-detail" UserDetail)
+  (.init t2))
+
+(defn home-page-2 []
   (println "I am in home page")
   (.registerComponent t2 "test-component" fun2)
   (.init t2))
