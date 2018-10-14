1adf29e7e452f3d4dc2d835dee6245ef2eaea281
Mohan <Mohankumarcm@gmail.com>
2018-10-14 11:10:13 +0800

    removed unnessary code

--- testaggrid-2/env/prod/clj/testaggrid_2/middleware.clj
+++
@@ -1,5 +0,0 @@
-(ns testaggrid-2.middleware
-  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
-
-(defn wrap-middleware [handler]
-  (wrap-defaults handler site-defaults))
--- testaggrid-2/env/prod/cljs/testaggrid_2/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns testaggrid-2.prod
-  (:require [testaggrid-2.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
